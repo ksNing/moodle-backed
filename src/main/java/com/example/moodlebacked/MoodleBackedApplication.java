d84c7492d5c9fd512ccb3d8d846ddfc669f77c20
zhangning <ning1994724@pku.edu.cn>
2019-11-27 17:17:43 +0800

    已达成war包部署到了阿里云服务器中

--- src/main/java/com/example/moodlebacked/MoodleBackedApplication.java
+++ src/main/java/com/example/moodlebacked/MoodleBackedApplication.java
@@ -2,9 +2,20 @@ package com.example.moodlebacked;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
-public class MoodleBackedApplication {
+public class MoodleBackedApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MoodleBackedApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoodleBackedApplication.class, args);
